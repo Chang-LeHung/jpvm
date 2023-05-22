22726e28517f5a10efdd26ad2c201451a8529fcd
changlehung <changlehung@qq.com>
2023-05-22 13:11:44 +0800

    update disassembler

--- src/main/java/org/jpvm/module/Disassembler.java
+++ src/main/java/org/jpvm/module/Disassembler.java
@@ -11,59 +11,59 @@ import java.util.Iterator;
 
 public class Disassembler {
 
-   private final CodeObject codeObject;
-   private final ByteCodeBuffer buf;
+  private final CodeObject codeObject;
+  private final ByteCodeBuffer buf;
 
-   public Disassembler(CodeObject codeObject) {
-      this.codeObject = codeObject;
-      buf = new ByteCodeBuffer(codeObject);
-   }
+  public Disassembler(CodeObject codeObject) {
+    this.codeObject = codeObject;
+    buf = new ByteCodeBuffer(codeObject);
+  }
 
-   public void dis() {
-      HashSet<Integer> enterPoint = new HashSet<>();
-      Iterator<Instruction> iterator = buf.iterator();
-      for (Instruction ins : buf) {
-         if (OpMap.instructions.containsKey(ins.getOpcode())) {
-            if (ins.getOpname().toString().toLowerCase().contains("jump"))
-               enterPoint.add(ins.getOparg());
-         }
+  public void dis() {
+    HashSet<Integer> enterPoint = new HashSet<>();
+    Iterator<Instruction> iterator = buf.iterator();
+    for (Instruction ins : buf) {
+      if (OpMap.instructions.containsKey(ins.getOpcode())) {
+        if (ins.getOpname().toString().toLowerCase().contains("jump"))
+          enterPoint.add(ins.getOparg());
       }
-      StringBuilder builder = new StringBuilder();
-      while (iterator.hasNext()) {
-         Instruction ins = iterator.next();
-         if (!OpMap.instructions.containsKey(ins.getOpcode())) // for bug fix
-            continue;
-         builder.delete(0, builder.length());
+    }
+    StringBuilder builder = new StringBuilder();
+    while (iterator.hasNext()) {
+      Instruction ins = iterator.next();
+      if (!OpMap.instructions.containsKey(ins.getOpcode())) // for bug fix
+        continue;
+      builder.delete(0, builder.length());
 
-         if (enterPoint.contains(ins.getPos())) {
-            builder.append(" >>");
-         }
-         builder.append("\t")
-             .append(String.format("%4d", ins.getPos()))
-             .append(" ")
-             .append(String.format("%-15s", ins.getOpname()))
-             .append("\t")
-             .append(String.format("%3d", ins.getOparg()));
-         switch (ins.getOpname()) {
-            case LOAD_CONST -> {
-               var coConsts = (PyTupleObject) codeObject.getCoConsts();
-               if (coConsts.get(ins.getOparg()) instanceof CodeObject cb) {
-                  builder.append(" <CodeObject ").append(cb.getCoName())
-                      .append(" @0x")
-                      .append(Integer.toHexString(System.identityHashCode(cb)))
-                      .append(" ")
-                      .append(cb.getCoFileName()).append(", line ")
-                      .append(cb.getCoFirstLineNo()).append(" >");
-               }else {
-                  builder.append(" (").append(coConsts.get(ins.getOparg())).append(")");
-               }
-            }
-            case STORE_NAME, LOAD_NAME, IMPORT_NAME -> {
-               var coNames = (PyTupleObject) codeObject.getCoNames();
-               builder.append(" (").append(coNames.get(ins.getOparg())).append(")");
-            }
-         }
-         System.out.println(builder);
+      if (enterPoint.contains(ins.getPos())) {
+        builder.append(" >>");
       }
-   }
+      builder.append("\t")
+          .append(String.format("%4d", ins.getPos()))
+          .append(" ")
+          .append(String.format("%-15s", ins.getOpname()))
+          .append("\t")
+          .append(String.format("%3d", ins.getOparg()));
+      switch (ins.getOpname()) {
+        case LOAD_CONST -> {
+          var coConsts = (PyTupleObject) codeObject.getCoConsts();
+          if (coConsts.get(ins.getOparg()) instanceof CodeObject cb) {
+            builder.append(" <CodeObject ").append(cb.getCoName())
+                .append(" @0x")
+                .append(Integer.toHexString(System.identityHashCode(cb)))
+                .append(" ")
+                .append(cb.getCoFileName()).append(", line ")
+                .append(cb.getCoFirstLineNo()).append(" >");
+          } else {
+            builder.append(" (").append(coConsts.get(ins.getOparg())).append(")");
+          }
+        }
+        case STORE_NAME, LOAD_NAME, IMPORT_NAME -> {
+          var coNames = (PyTupleObject) codeObject.getCoNames();
+          builder.append(" (").append(coNames.get(ins.getOparg())).append(")");
+        }
+      }
+      System.out.println(builder);
+    }
+  }
 }
