24141f4c551d34d55c41ca2a0a9e24b0862e6a6d
xiaoyume <1726009889@qq.com>
2023-06-01 22:59:32 +0800

    fix sout cursor

--- src/test/java/org/jpvm/objects/PyLongObjectTest.java
+++ src/test/java/org/jpvm/objects/PyLongObjectTest.java
@@ -14,7 +14,7 @@ public class PyLongObjectTest {
         System.out.println(res.repr());
         assert res.getData() == 2;
         Assert.assertEquals(res.getData(), 2);
-        System.out.println("\u001B[32m method add of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method add of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -25,7 +25,7 @@ public class PyLongObjectTest {
         System.out.println(res.repr());
         assert res.getData() == 1;
 //        Assert.assertEquals(res.getData(), 1);
-        System.out.println("\u001B[32m method sub of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method sub of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -35,7 +35,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.mul(o2);
         System.out.println(res.repr());
         assert res.getData() == 6;
-        System.out.println("\u001B[32m method mul of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method mul of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -45,7 +45,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o2.mod(o1);
         System.out.println(res.repr());
         assert res.getData() == 1;
-        System.out.println("\u001B[32m method mod of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method mod of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -56,7 +56,7 @@ public class PyLongObjectTest {
         System.out.println(res);
         assert res.get(0).equals(2.5);
         assert res.get(1).equals(2);
-        System.out.println("\u001B[32m method divmod of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method divmod of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -66,7 +66,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o2.pow(o1);
         System.out.println(res.repr());
         assert res.getData() == 9;
-        System.out.println("\u001B[32m method pow of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method pow of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -75,7 +75,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.neg();
         System.out.println(res.repr());
         assert res.getData() == -1;
-        System.out.println("\u001B[32m method neg of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method neg of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -84,7 +84,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.pos();
         System.out.println(res.repr());
         assert res.getData() == 1;
-        System.out.println("\u001B[32m method pos of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method pos of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -93,7 +93,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.abs();
         System.out.println(res.repr());
         assert res.getData() == 1;
-        System.out.println("\u001B[32m method abs of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method abs of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -102,7 +102,7 @@ public class PyLongObjectTest {
         var res = (PyBoolObject)o1.bool();
         System.out.println(res);
         assert res.isTrue();
-        System.out.println("\u001B[32m method bool of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method bool of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -111,7 +111,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.invert();
         System.out.println(res.repr());
         assert res.getData() == -2;
-        System.out.println("\u001B[32m method invert of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method invert of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -121,7 +121,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.lshift(o2);
         System.out.println(res.repr());
         assert res.getData() == 4;
-        System.out.println("\u001B[32m method lshift of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method lshift of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -131,7 +131,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.rshift(o2);
         System.out.println(res.repr());
         assert res.getData() == 2;
-        System.out.println("\u001B[32m method rshift of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method rshift of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -141,7 +141,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.and(o2);
         System.out.println(res.repr());
         assert res.getData() == 0;
-        System.out.println("\u001B[32m method and of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method and of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -151,7 +151,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.xor(o2);
         System.out.println(res.repr());
         assert res.getData() == 6;
-        System.out.println("\u001B[32m method xor of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method xor of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -161,7 +161,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.or(o2);
         System.out.println(res.repr());
         assert res.getData() == 7;
-        System.out.println("\u001B[32m method or of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method or of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -170,7 +170,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.nbInt();
         System.out.println(res.repr());
         assert res.getData() == 4;
-        System.out.println("\u001B[32m method nbInt of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method nbInt of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -179,7 +179,7 @@ public class PyLongObjectTest {
         var res = (PyFloatObject)o1.nbFloat();
         System.out.println(res.repr());
         assert res.getData() == 4.0;
-        System.out.println("\u001B[32m method nbFloat of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method nbFloat of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -189,7 +189,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.floorDiv(o2);
         System.out.println(res.repr());
         assert res.getData() == 1;
-        System.out.println("\u001B[32m method floorDiv of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method floorDiv of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -199,7 +199,7 @@ public class PyLongObjectTest {
         var res = (PyFloatObject)o1.trueDiv(o2);
         System.out.println(res.repr());
         assert res.getData() == 2.5;
-        System.out.println("\u001B[32m method trueDiv of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method trueDiv of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -208,7 +208,7 @@ public class PyLongObjectTest {
         var res = (PyLongObject)o1.index();
         System.out.println(res.repr());
         assert res.getData() == 4;
-        System.out.println("\u001B[32m method index of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method index of PyLongObject pass\u001B[0m");
     }
 
     @Test
@@ -218,6 +218,6 @@ public class PyLongObjectTest {
         var res = o1.equals(o2);
         System.out.println(res);
         assert res == false;
-        System.out.println("\u001B[32m method equals of PyLongObject pass\u001B[32m");
+        System.out.println("\u001B[32m method equals of PyLongObject pass\u001B[0m");
     }
 }
