ce2049194e17bde5cfa8b34e1897af99e3e3c1c5
changlehung <changlehung@qq.com>
2023-07-28 20:09:18 +0800

    update shutil module

--- src/main/java/org/jpvm/stl/shutil/PyModuleMain.java
+++ src/main/java/org/jpvm/stl/shutil/PyModuleMain.java
@@ -1,11 +1,65 @@
 package org.jpvm.stl.shutil;
 
-import org.jpvm.objects.PyModuleObject;
-import org.jpvm.objects.PyUnicodeObject;
+import org.jpvm.errors.PyException;
+import org.jpvm.excptions.PyErrorUtils;
+import org.jpvm.objects.*;
+import org.jpvm.objects.annotation.PyClassMethod;
+import org.jpvm.python.BuiltIn;
+
+import java.io.IOException;
+import java.nio.file.*;
 
 public class PyModuleMain extends PyModuleObject {
 
-	public PyModuleMain(PyUnicodeObject moduleName) {
-		super(moduleName);
-	}
+  public void copyFile(Path source, Path destination) throws IOException {
+    Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+  }
+
+  public void copyDirectory(Path source, Path destination) throws IOException, IOException {
+    Files.createDirectories(destination);
+    try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(source)) {
+      for (Path entry : directoryStream) {
+        Path targetEntry = destination.resolve(entry.getFileName());
+        if (Files.isDirectory(entry)) {
+          copyDirectory(entry, targetEntry);
+        } else {
+          copyFile(entry, targetEntry);
+        }
+      }
+    }
+  }
+
+  @PyClassMethod
+  public PyObject copytree(PyTupleObject args, PyDictObject kwArgs) throws PyException {
+    if (args.size() != 2) {
+      throw new RuntimeException("copytree() takes two arguments");
+    }
+    Path source = Paths.get(args.get(0).toString());
+    Path destination = Paths.get(args.get(1).toString());
+    try {
+      copyDirectory(source, destination);
+    } catch (IOException e) {
+      return PyErrorUtils.pyErrorFormat(PyErrorUtils.RuntimeError, e.getMessage());
+    }
+    return BuiltIn.None;
+  }
+
+  @PyClassMethod
+  public PyObject copyfile(PyTupleObject args, PyDictObject kwArgs) throws PyException {
+    if (args.size() != 2) {
+      throw new RuntimeException("copytree() takes two arguments");
+    }
+    Path source = Paths.get(args.get(0).toString());
+    Path destination = Paths.get(args.get(1).toString());
+    try {
+      copyFile(source, destination);
+    } catch (IOException e) {
+      return PyErrorUtils.pyErrorFormat(PyErrorUtils.RuntimeError, e.getMessage());
+    }
+    return BuiltIn.None;
+  }
+
+  public PyModuleMain(PyUnicodeObject moduleName) {
+    super(moduleName);
+  }
 }
--- src/main/java/org/jpvm/stl/url/PyParseResultObject.java
+++ src/main/java/org/jpvm/stl/url/PyParseResultObject.java
@@ -2,42 +2,45 @@ package org.jpvm.stl.url;
 
 import org.jpvm.objects.PyLongObject;
 import org.jpvm.objects.PyModuleObject;
+import org.jpvm.objects.PyObject;
 import org.jpvm.objects.PyUnicodeObject;
 import org.jpvm.objects.annotation.PyClassAttribute;
 
-public class PyParseResultObject extends PyModuleObject {
-    @PyClassAttribute
-    private PyUnicodeObject protocol;
-    @PyClassAttribute
-    private PyUnicodeObject host;
-    @PyClassAttribute
-    private PyLongObject port;
-    @PyClassAttribute
-    private PyUnicodeObject path;
-    @PyClassAttribute
-    private PyUnicodeObject query;
-    @PyClassAttribute
-    private PyUnicodeObject fragment;
+public class PyParseResultObject extends PyObject {
+  @PyClassAttribute private PyUnicodeObject protocol;
+  @PyClassAttribute private PyUnicodeObject host;
+  @PyClassAttribute private PyLongObject port;
+  @PyClassAttribute private PyUnicodeObject path;
+  @PyClassAttribute private PyUnicodeObject query;
+  @PyClassAttribute private PyUnicodeObject fragment;
 
-    public PyParseResultObject(String protocol, String host, int port, String path, String query, String fragment) {
-        super();
-        this.protocol = new PyUnicodeObject(protocol);
-        this.host = new PyUnicodeObject(host);
-        this.port = PyLongObject.getLongObject(port);
-        this.path = new PyUnicodeObject(path);
-        this.query = new PyUnicodeObject(query);
-        this.fragment = new PyUnicodeObject(fragment);
-    }
+  public PyParseResultObject(
+      String protocol, String host, int port, String path, String query, String fragment) {
+    this.protocol = new PyUnicodeObject(protocol);
+    this.host = new PyUnicodeObject(host);
+    this.port = PyLongObject.getLongObject(port);
+    this.path = new PyUnicodeObject(path);
+    this.query = new PyUnicodeObject(query);
+    this.fragment = new PyUnicodeObject(fragment);
+  }
 
-    @Override
-    public PyUnicodeObject str() {
+  @Override
+  public PyUnicodeObject str() {
 
-        return new PyUnicodeObject(new String("ParseResult(protocol='"+ protocol +
-                "', host='" + host +
-                "', port='" + port +
-                "', path='" + path +
-                "', query='" + query +
-                "', fragment='" + fragment + "')"
-        ));
-    }
+    return new PyUnicodeObject(
+        new String(
+            "ParseResult(protocol='"
+                + protocol
+                + "', host='"
+                + host
+                + "', port='"
+                + port
+                + "', path='"
+                + path
+                + "', query='"
+                + query
+                + "', fragment='"
+                + fragment
+                + "')"));
+  }
 }
