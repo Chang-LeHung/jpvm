085812358fd20e491a045bed3262e58d20331ee2
changlehung <changlehung@qq.com>
2023-11-12 17:27:35 +0800

    update threading PyConditionObject

--- src/main/java/org/jpvm/stl/threading/PyConditionObjectType.java
+++ src/main/java/org/jpvm/stl/threading/PyConditionObjectType.java
@@ -1,9 +1,18 @@
 package org.jpvm.stl.threading;
 
+import org.jpvm.excptions.jobjs.PyException;
+import org.jpvm.objects.PyDictObject;
+import org.jpvm.objects.PyObject;
+import org.jpvm.objects.PyTupleObject;
 import org.jpvm.objects.types.PyTypeType;
 
 public class PyConditionObjectType extends PyTypeType {
-	public PyConditionObjectType(Class<?> clazz) {
-		super(clazz);
-	}
+  public PyConditionObjectType(Class<?> clazz) {
+    super(clazz);
+  }
+
+  @Override
+  public PyObject call(PyTupleObject args, PyDictObject kwArgs) throws PyException {
+    return new PyConditionObject();
+  }
 }
