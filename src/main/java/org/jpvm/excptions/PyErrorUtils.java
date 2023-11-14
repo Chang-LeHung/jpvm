6f2ed83eba43b7b0ed866293a6005125f79bbc42
changlehung <changlehung@qq.com>
2023-11-14 22:51:37 +0800

    update some fields of PyErrorUtils to final

--- src/main/java/org/jpvm/excptions/PyErrorUtils.java
+++ src/main/java/org/jpvm/excptions/PyErrorUtils.java
@@ -9,25 +9,25 @@ import org.jpvm.vm.ThreadState;
 
 public class PyErrorUtils {
 
-  public static PyBaseExceptionType BaseException = new PyBaseExceptionType();
+  public static final PyBaseExceptionType BaseException = new PyBaseExceptionType();
 
-  public static PyBaseExceptionType Exception = new PyExceptionType();
-  public static PyBaseExceptionType AssertionError = new PyAssertionErrorType();
-  public static PyBaseExceptionType AttributeError = new PyAttributeErrorType();
-  public static PyBaseExceptionType KeyError = new PyKeyErrorType();
-  public static PyBaseExceptionType NameError = new PyNameErrorType();
-  public static PyBaseExceptionType NotImplementedError = new PyNotImplementedErrorType();
-  public static PyBaseExceptionType RuntimeError = new PyRuntimeErrorType();
-  public static PyBaseExceptionType TypeError = new PyTypeErrorType();
-  public static PyBaseExceptionType ValueError = new PyValueErrorType();
-  public static PyBaseExceptionType ZeroDivisionError = new PyZeroDivisionErrorType();
+  public static final PyBaseExceptionType Exception = new PyExceptionType();
+  public static final PyBaseExceptionType AssertionError = new PyAssertionErrorType();
+  public static final PyBaseExceptionType AttributeError = new PyAttributeErrorType();
+  public static final PyBaseExceptionType KeyError = new PyKeyErrorType();
+  public static final PyBaseExceptionType NameError = new PyNameErrorType();
+  public static final PyBaseExceptionType NotImplementedError = new PyNotImplementedErrorType();
+  public static final PyBaseExceptionType RuntimeError = new PyRuntimeErrorType();
+  public static final PyBaseExceptionType TypeError = new PyTypeErrorType();
+  public static final PyBaseExceptionType ValueError = new PyValueErrorType();
+  public static final PyBaseExceptionType ZeroDivisionError = new PyZeroDivisionErrorType();
 
-  public static PyBaseExceptionType StackOverflowError = new PyStackOverflowType();
+  public static final PyBaseExceptionType StackOverflowError = new PyStackOverflowType();
 
-  public static PyImportErrorType ImportError = new PyImportErrorType();
-  public static PyIndexOutOfBoundErrorType IndexError = new PyIndexOutOfBoundErrorType();
-  public static PyFileNotFoundErrorType FileNotFoundError = new PyFileNotFoundErrorType();
-  public static PyStopIterationType StopIteration = new PyStopIterationType();
+  public static final PyImportErrorType ImportError = new PyImportErrorType();
+  public static final PyIndexOutOfBoundErrorType IndexError = new PyIndexOutOfBoundErrorType();
+  public static final PyFileNotFoundErrorType FileNotFoundError = new PyFileNotFoundErrorType();
+  public static final PyStopIterationType StopIteration = new PyStopIterationType();
 
   public static PyObject pyErrorFormat(PyBaseExceptionType type, String msg) throws PyException {
     // create an instance of the exception
