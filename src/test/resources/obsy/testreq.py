b9260e4daf8efa9bb68341d7af1959b19e9952d3
changlehung <changlehung@qq.com>
2023-11-15 01:33:25 +0800

    eliminate disruptive output from the console

--- src/test/resources/obsy/testreq.py
+++ src/test/resources/obsy/testreq.py
@@ -2,13 +2,13 @@ import url
 from url import urlopen, urlretrieve, urlparse
 
 ur = urlopen("https://www.baidu.com")
-print(ur)
+# print(ur)
 
 # urlretrieve("https://www.baidu.com", "src/test/resources/obsy/test.txt");
 
 
 o = urlparse("https://example.com:8080/path/to/page?query=java&category=tutorial#section1")
-print(o)
+# print(o)
 # print(o.port)
 # print(o.protocol)
 # print(o.host)
