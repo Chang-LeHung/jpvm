2454ec85dd884dc23f7d7867b1fc1d7ce8dd2d68
changlehung <changlehung@qq.com>
2023-11-16 14:41:14 +0800

    add magic __str__ __repr__ test cases

---
+++ src/test/java/org/jpvm/magicmethod/MagicMethod.java
@@ -0,0 +1,20 @@
+package org.jpvm.magicmethod;
+
+import org.jpvm.exceptions.jobjs.PyException;
+import org.jpvm.vm.JPVM;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class MagicMethod {
+
+  public void run(String filename) throws PyException, IOException {
+    JPVM jpvm = new JPVM(filename);
+    jpvm.run();
+  }
+
+  @Test
+  public void testStrRepr() throws PyException, IOException {
+    run("src/test/resources/magicmethod/__pycache__/str_repr.cpython-38.pyc");
+  }
+}
