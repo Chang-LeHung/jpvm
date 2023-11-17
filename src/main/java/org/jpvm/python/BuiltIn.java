41b9a9d6e6041c505af6960b66afd3ecfd282a7d
changlehung <changlehung@qq.com>
2023-11-17 15:11:49 +0800

    update builtin

--- src/main/java/org/jpvm/python/BuiltIn.java
+++ src/main/java/org/jpvm/python/BuiltIn.java
@@ -208,8 +208,7 @@ public class BuiltIn {
         return map.mpLength(null);
       }
     }
-    PyErrorUtils.pyErrorFormat(PyErrorUtils.TypeError, "sum only require one iterable argument");
-    return null;
+    return PyErrorUtils.pyErrorFormat(PyErrorUtils.TypeError, "sum only require one iterable argument");
   }
 
   public static PyObject sorted(PyTupleObject args, PyDictObject kwArgs) throws PyException {
@@ -277,9 +276,8 @@ public class BuiltIn {
         return iterator.next();
       }
     }
-    PyErrorUtils.pyErrorFormat(
+    return PyErrorUtils.pyErrorFormat(
         PyErrorUtils.TypeError, "next function require only 1 argument that is an iterator");
-    return null;
   }
 
   public static PyObject __build_class__(PyTupleObject args, PyDictObject kwArgs)
@@ -325,7 +323,7 @@ public class BuiltIn {
       out = PyUnicodeObject.getOrCreateFromInternStringPool(str, false);
       return out;
     }
-    throw new PyException("input function require at most 1 argument");
+    return PyErrorUtils.pyErrorFormat(PyErrorUtils.TypeError, "input function require at most 1 argument");
   }
 
   public static PyObject open(PyTupleObject args, PyDictObject kwArgs) throws PyException {
