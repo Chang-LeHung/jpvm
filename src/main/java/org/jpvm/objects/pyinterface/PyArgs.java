dbc9d1ff65d9969b1465dfaf726f2aad893e8080
changlehung <changlehung@qq.com>
2023-11-19 18:38:23 +0800

    update PyArgs'

--- src/main/java/org/jpvm/objects/pyinterface/PyArgs.java
+++ src/main/java/org/jpvm/objects/pyinterface/PyArgs.java
@@ -3,8 +3,7 @@ package org.jpvm.objects.pyinterface;
 public interface PyArgs {
 
   /**
-   * this method is used for transforming {@link org.jpvm.objects.PyObject}
-   * to native Java type
+   * this method is used for transforming {@link org.jpvm.objects.PyObject} to native Java type
    *
    * @return {@link Object}
    */
