f501d9ae82211e81287db57dd2945195adcb1273
FLCZ <FLCZ@qq.com>
2024-03-08 09:19:34 +0800

    去除pystring，替换为pyunicodeobject

--- src/main/java/org/jpvm/stl/io/PyString.java
+++
@@ -1,9 +0,0 @@
-package org.jpvm.stl.io;
-
-import org.jpvm.objects.PyObject;
-
-public class PyString extends PyObject {
-    String string;
-    public PyString(){}
-    public String toString() {return string;}
-}
