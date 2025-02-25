ce9b3ce16cdad1f7b14a91d19e83cb5374e349fe
changlehung <changlehung@gmail.com>
2025-02-26 01:24:04 +0800

    feat: expose Volatile and Atomic classes

--- src/main/java/org/jpvm/stl/atomic/PyModuleMain.java
+++ src/main/java/org/jpvm/stl/atomic/PyModuleMain.java
@@ -2,9 +2,23 @@ package org.jpvm.stl.atomic;
 
 import org.jpvm.objects.PyModuleObject;
 import org.jpvm.objects.PyUnicodeObject;
+import org.jpvm.objects.annotation.PyClassAttribute;
 
 public class PyModuleMain extends PyModuleObject {
+
+  @PyClassAttribute
+  private final PyVolatileType Volatile;
+
+  @PyClassAttribute
+  private final PyAtomicLongType AtomicLong;
+
+  @PyClassAttribute
+  private final PyAtomicLongType AtomicReference;
+
   public PyModuleMain(PyUnicodeObject moduleName) {
     super(moduleName);
+    Volatile = PyVolatileType.getInstance();
+    AtomicLong = PyAtomicLongType.getInstance();
+    AtomicReference = PyAtomicLongType.getInstance();
   }
 }
